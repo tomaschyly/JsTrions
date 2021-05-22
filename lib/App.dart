e4aadfb6be9bfd2b46ac25dba37ad8b6515ca836
tomaschyly <chyly@tomas-chyly.com>
2021-05-22 21:15:13 +0200

    Initialization UI

--- lib/App.dart
+++ lib/App.dart
@@ -1,7 +1,9 @@
 import 'dart:io';
 
 import 'package:js_trions/core/Router.dart' as AppRouter;
+import 'package:js_trions/images/TomasChyly.dart';
 import 'package:js_trions/ui/DashboardScreen.dart';
+import 'package:js_trions/utils/AppTheme.dart';
 import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:sqflite/sqflite.dart';
@@ -37,7 +39,24 @@ class AppState extends AbstractStatefulWidgetState<App> {
             backgroundColor: Theme.of(context).backgroundColor,
             body: Container(
               child: Center(
-                child: Text('WIP'), //TODO UI
+                child: Padding(
+                  padding: const EdgeInsets.all(AppDimens.PrimaryMargin),
+                  child: Container(
+                    width: 295,
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        tomasChylyImage,
+                        Container(height: AppDimens.PrimaryVerticalMargin),
+                        Text(
+                          'JsTrions\nby Tomáš Chylý',
+                          // style: AppStyles.TextHeadline.copyWith(fontFamily: 'CaveHand'), //TODO theme
+                          textAlign: TextAlign.center,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
               ),
             ),
           );
