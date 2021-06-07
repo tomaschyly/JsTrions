8557146b3ceb5a9133a605d4f5d4da17eadfc688
tomaschyly <chyly@tomas-chyly.com>
2021-06-07 20:49:08 +0200

    Update main dependencies

--- lib/ui/DashboardScreen.dart
+++ lib/ui/DashboardScreen.dart
@@ -1,8 +1,5 @@
-import 'package:flutter_svg/flutter_svg.dart';
-import 'package:js_trions/core/AppTheme.dart';
 import 'package:js_trions/ui/screenStates/AppResponsiveScreenState.dart';
 import 'package:tch_appliable_core/tch_appliable_core.dart';
-import 'package:tch_common_widgets/tch_common_widgets.dart';
 
 class DashboardScreen extends AbstractResposiveScreen {
   static const String ROUTE = "/dashboard";
