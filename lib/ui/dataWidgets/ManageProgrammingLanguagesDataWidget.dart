2f47a1dcb99a6c13706668324dc618127d8ccf8c
tomaschyly <chyly@tomas-chyly.com>
2021-06-08 23:29:46 +0200

    Chips for existing ProgrammingLanguages

--- lib/ui/dataWidgets/ManageProgrammingLanguagesDataWidget.dart
+++ lib/ui/dataWidgets/ManageProgrammingLanguagesDataWidget.dart
@@ -1,6 +1,9 @@
+import 'package:js_trions/core/AppTheme.dart';
+import 'package:js_trions/model/ProgrammingLanguage.dart';
 import 'package:js_trions/model/ProgrammingLanguages.dart';
 import 'package:js_trions/model/dataRequests/GetProgrammingLanguagesDataRequest.dart';
 import 'package:tch_appliable_core/tch_appliable_core.dart';
+import 'package:tch_common_widgets/tch_common_widgets.dart';
 
 class ManageProgrammingLanguagesDataWidget extends AbstractDataWidget {
   /// ManageProgrammingLanguagesDataWidget initialization
@@ -29,8 +32,57 @@ class _ManageProgrammingLanguagesDataWidgetState extends AbstractDataWidgetState
           return Container();
         }
 
-        return Container(); //TODO
+        return Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Wrap(
+              spacing: kCommonHorizontalMarginHalf,
+              runSpacing: kCommonVerticalMargin,
+              children: programmingLanguages.programmingLanguages
+                  .map((ProgrammingLanguage programmingLanguage) => _ChipWidget(programmingLanguage: programmingLanguage))
+                  .toList(),
+            ),
+            CommonSpaceV(),
+          ],
+        );
       },
+    );
+  }
+}
+
+class _ChipWidget extends StatelessWidget {
+  final ProgrammingLanguage programmingLanguage;
+
+  /// ChipWidget initialization
+  _ChipWidget({
+    required this.programmingLanguage,
+  });
+
+  /// Create view layout from widgets
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: kButtonHeight,
+      padding: const EdgeInsets.symmetric(horizontal: kCommonHorizontalMarginHalf),
+      decoration: BoxDecoration(
+        border: Border.all(
+          width: 1,
+          color: kColorTextPrimary,
+        ),
+        borderRadius: BorderRadius.circular(8),
+      ),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text(
+            programmingLanguage.name,
+            style: fancyText(kText),
+          ),
+        ],
+      ),
     );
   }
 }
