47ba777f0b047301267ac3ec5ddc861921b2d180
tomaschyly <chyly@tomas-chyly.com>
2021-06-23 12:05:22 +0200

    ToggleContainerWidget component

---
+++ lib/ui/widgets/ToggleContainerWidget.dart
@@ -0,0 +1,82 @@
+import 'package:js_trions/core/AppTheme.dart';
+import 'package:tch_appliable_core/tch_appliable_core.dart';
+import 'package:tch_common_widgets/tch_common_widgets.dart';
+
+class ToggleContainerWidget extends AbstractStatefulWidget {
+  final String title;
+  final Widget content;
+
+  /// ToggleContainerWidget initialization
+  ToggleContainerWidget({
+    required this.title,
+    required this.content,
+  });
+
+  /// Create state for widget
+  @override
+  State<StatefulWidget> createState() => _ToggleContainerWidgetState();
+}
+
+class _ToggleContainerWidgetState extends AbstractStatefulWidgetState<ToggleContainerWidget> with TickerProviderStateMixin {
+  bool _isOpen = false;
+
+  /// Create view layout from widgets
+  @override
+  Widget buildContent(BuildContext context) {
+    final commonTheme = CommonTheme.of<AppTheme>(context)!;
+
+    return ClipRRect(
+      borderRadius: commonTheme.buttonsStyle.buttonStyle.borderRadius,
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: commonTheme.buttonsStyle.buttonStyle.borderRadius,
+          border: Border.all(
+            width: 1,
+            color: commonTheme.buttonsStyle.buttonStyle.color ?? Colors.transparent,
+          ),
+        ),
+        child: AnimatedSize(
+          duration: kThemeAnimationDuration,
+          vsync: this,
+          alignment: Alignment.topCenter,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  CommonSpaceHHalf(),
+                  Text(
+                    widget.title,
+                    style: fancyText(kTextBold),
+                  ),
+                  CommonSpaceH(),
+                  Spacer(),
+                  IconButtonWidget(
+                    style: commonTheme.buttonsStyle.iconButtonStyle.copyWith(
+                      variant: IconButtonVariant.IconOnly,
+                    ),
+                    svgAssetPath: _isOpen ? 'images/chevron-up.svg' : 'images/chevron-down.svg',
+                    onTap: () {
+                      setStateNotDisposed(() {
+                        _isOpen = !_isOpen;
+                      });
+                    },
+                  ),
+                ],
+              ),
+              if (_isOpen) ...[
+                CommonSpaceV(),
+                widget.content,
+              ],
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
