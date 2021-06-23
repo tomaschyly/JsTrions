757857b1441a54cfcd50ad439714981c0f821149
tomaschyly <chyly@tomas-chyly.com>
2021-06-24 00:10:09 +0200

    AboutScreen responsive layout, logo, version

--- lib/ui/screens/AboutScreen.dart
+++ lib/ui/screens/AboutScreen.dart
@@ -1,5 +1,8 @@
+import 'package:js_trions/core/AppTheme.dart';
 import 'package:js_trions/ui/screenStates/AppResponsiveScreenState.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 import 'package:tch_appliable_core/tch_appliable_core.dart';
+import 'package:tch_common_widgets/tch_common_widgets.dart';
 
 class AboutScreen extends AbstractResposiveScreen {
   static const String ROUTE = "/about";
@@ -17,16 +20,16 @@ class _AboutScreenState extends AppResponsiveScreenState<AboutScreen> {
   );
 
   @override
-  Widget extraLargeDesktopScreen(BuildContext context) => _BodyWidget();
+  Widget extraLargeDesktopScreen(BuildContext context) => _BodyDesktopWidget();
 
   @override
-  Widget largeDesktopScreen(BuildContext context) => _BodyWidget();
+  Widget largeDesktopScreen(BuildContext context) => _BodyDesktopWidget();
 
   @override
   Widget largePhoneScreen(BuildContext context) => _BodyWidget();
 
   @override
-  Widget smallDesktopScreen(BuildContext context) => _BodyWidget();
+  Widget smallDesktopScreen(BuildContext context) => _BodyDesktopWidget();
 
   @override
   Widget smallPhoneScreen(BuildContext context) => _BodyWidget();
@@ -38,12 +41,71 @@ class _AboutScreenState extends AppResponsiveScreenState<AboutScreen> {
 abstract class _AbstractBodyWidget extends AbstractStatefulWidget {}
 
 abstract class _AbstractBodyWidgetState<T extends _AbstractBodyWidget> extends AbstractStatefulWidgetState<T> {
+  String _version = '';
+
+  /// Run initializations of screen on first build only
+  @override
+  firstBuildOnly(BuildContext context) {
+    super.firstBuildOnly(context);
+
+    PackageInfo.fromPlatform().then((PackageInfo packageInfo) => setStateNotDisposed(() {
+          _version = 'JsTrions: ${packageInfo.version}';
+        }));
+  }
+
   /// Create view layout from widgets
   @override
   Widget buildContent(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text('Wip: about'),
+    return Scrollbar(
+      child: SingleChildScrollView(
+        child: Container(
+          width: double.infinity,
+          padding: const EdgeInsets.symmetric(horizontal: kCommonHorizontalMargin),
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              CommonSpaceV(),
+              Container(
+                width: kPhoneStopBreakpoint,
+                padding: const EdgeInsets.symmetric(horizontal: kCommonHorizontalMargin),
+                child: Center(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Container(
+                        width: 295,
+                        child: Image.asset('images/tomas-chylyV3-padded-black-circle.png'),
+                      ),
+                      CommonSpaceVHalf(),
+                      Text(
+                        _version,
+                        style: fancyText(kText),
+                        textAlign: TextAlign.center,
+                      ),
+                      CommonSpaceV(),
+                    ],
+                  ),
+                ),
+              ),
+              Container(
+                width: kPhoneStopBreakpoint,
+                padding: const EdgeInsets.symmetric(horizontal: kCommonHorizontalMargin),
+                child: _InfoWidget(),
+              ),
+              Container(
+                width: kPhoneStopBreakpoint,
+                padding: const EdgeInsets.symmetric(horizontal: kCommonHorizontalMargin),
+                child: _LinksWidget(),
+              ),
+              Container(
+                width: kPhoneStopBreakpoint,
+                padding: const EdgeInsets.symmetric(horizontal: kCommonHorizontalMargin),
+                child: _AttributionWidget(),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
@@ -56,3 +118,126 @@ class _BodyWidget extends _AbstractBodyWidget {
 }
 
 class _BodyWidgetState extends _AbstractBodyWidgetState<_BodyWidget> {}
+
+class _BodyDesktopWidget extends _AbstractBodyWidget {
+  /// Create state for widget
+  @override
+  State<StatefulWidget> createState() => _BodyDesktopWidgetState();
+}
+
+class _BodyDesktopWidgetState extends _AbstractBodyWidgetState<_BodyDesktopWidget> {
+  /// Create view layout from widgets
+  @override
+  Widget buildContent(BuildContext context) {
+    return Scrollbar(
+      child: SingleChildScrollView(
+        child: Container(
+          width: double.infinity,
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              CommonSpaceV(),
+              Row(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Expanded(
+                    child: Container(
+                      alignment: Alignment.centerLeft,
+                      child: Container(
+                        width: kPhoneStopBreakpoint,
+                        padding: const EdgeInsets.symmetric(horizontal: kCommonHorizontalMargin),
+                        child: Center(
+                          child: Column(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              Container(
+                                width: 295,
+                                child: Image.asset('images/tomas-chylyV3-padded-black-circle.png'),
+                              ),
+                              CommonSpaceVHalf(),
+                              Text(
+                                _version,
+                                style: fancyText(kText),
+                                textAlign: TextAlign.center,
+                              ),
+                              CommonSpaceV(),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(),
+                  ),
+                ],
+              ),
+              CommonSpaceV(),
+              Row(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Expanded(
+                    child: Container(
+                      alignment: Alignment.centerLeft,
+                      child: Container(
+                        width: kPhoneStopBreakpoint,
+                        padding: const EdgeInsets.symmetric(horizontal: kCommonHorizontalMargin),
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            _InfoWidget(),
+                            _AttributionWidget(),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      alignment: Alignment.centerLeft,
+                      child: Container(
+                        width: kPhoneStopBreakpoint,
+                        padding: const EdgeInsets.symmetric(horizontal: kCommonHorizontalMargin),
+                        child: _LinksWidget(),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _InfoWidget extends StatelessWidget {
+  /// Create view layout from widgets
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+class _LinksWidget extends StatelessWidget {
+  /// Create view layout from widgets
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+class _AttributionWidget extends StatelessWidget {
+  /// Create view layout from widgets
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,6 +6,7 @@ import FlutterMacOS
 import Foundation
 
 import file_selector_macos
+import package_info_plus_macos
 import path_provider_macos
 import shared_preferences_macos
 import sqflite
@@ -13,6 +14,7 @@ import tch_common_widgets
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
+  FLTPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FLTPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
