aa5b95dd9da7f16d2834f5e165b148b1715d5b6e
tomaschyly <chyly@tomas-chyly.com>
2021-06-23 12:14:14 +0200

    Improve ToggleContainerWidget component

--- lib/ui/widgets/ToggleContainerWidget.dart
+++ lib/ui/widgets/ToggleContainerWidget.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_svg/svg.dart';
 import 'package:js_trions/core/AppTheme.dart';
 import 'package:tch_appliable_core/tch_appliable_core.dart';
 import 'package:tch_common_widgets/tch_common_widgets.dart';
@@ -44,30 +45,45 @@ class _ToggleContainerWidgetState extends AbstractStatefulWidgetState<ToggleCont
             mainAxisAlignment: MainAxisAlignment.start,
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Row(
-                mainAxisSize: MainAxisSize.max,
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  CommonSpaceHHalf(),
-                  Text(
-                    widget.title,
-                    style: fancyText(kTextBold),
-                  ),
-                  CommonSpaceH(),
-                  Spacer(),
-                  IconButtonWidget(
-                    style: commonTheme.buttonsStyle.iconButtonStyle.copyWith(
-                      variant: IconButtonVariant.IconOnly,
+              Material(
+                color: Colors.transparent,
+                child: InkWell(
+                  child: Container(
+                    height: commonTheme.buttonsStyle.iconButtonStyle.height,
+                    padding: const EdgeInsets.only(left: kCommonHorizontalMarginHalf),
+                    alignment: Alignment.center,
+                    child: Row(
+                      mainAxisSize: MainAxisSize.max,
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Text(
+                          widget.title,
+                          style: fancyText(kTextBold),
+                        ),
+                        CommonSpaceH(),
+                        Spacer(),
+                        Container(
+                          width: commonTheme.buttonsStyle.iconButtonStyle.width,
+                          height: commonTheme.buttonsStyle.iconButtonStyle.height,
+                          child: Center(
+                            child: SvgPicture.asset(
+                              _isOpen ? 'images/chevron-up.svg' : 'images/chevron-down.svg',
+                              width: commonTheme.buttonsStyle.iconButtonStyle.iconWidth,
+                              height: commonTheme.buttonsStyle.iconButtonStyle.iconHeight,
+                              color: commonTheme.buttonsStyle.iconButtonStyle.color,
+                            ),
+                          ),
+                        ),
+                      ],
                     ),
-                    svgAssetPath: _isOpen ? 'images/chevron-up.svg' : 'images/chevron-down.svg',
-                    onTap: () {
-                      setStateNotDisposed(() {
-                        _isOpen = !_isOpen;
-                      });
-                    },
                   ),
-                ],
+                  onTap: () {
+                    setStateNotDisposed(() {
+                      _isOpen = !_isOpen;
+                    });
+                  },
+                ),
               ),
               if (_isOpen) ...[
                 CommonSpaceV(),
