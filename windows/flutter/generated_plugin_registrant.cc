1a762444e485b2569ac4a70befbb56c6ad97f5e9
tomaschyly <chyly@tomas-chyly.com>
2021-09-24 21:29:58 +0200

    Packages updated

--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -8,13 +8,13 @@
 
 #include <file_selector_windows/file_selector_plugin.h>
 #include <tch_common_widgets/tch_common_widgets_plugin.h>
-#include <url_launcher_windows/url_launcher_plugin.h>
+#include <url_launcher_windows/url_launcher_windows.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
   FileSelectorPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("FileSelectorPlugin"));
   TchCommonWidgetsPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("TchCommonWidgetsPlugin"));
-  UrlLauncherPluginRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("UrlLauncherPlugin"));
+  UrlLauncherWindowsRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("UrlLauncherWindows"));
 }
