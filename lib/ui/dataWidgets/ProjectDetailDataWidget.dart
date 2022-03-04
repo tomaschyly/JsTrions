2b2463c64aa7906ec2780acd7b6bdec193dff0f7
tomaschyly <chyly@tomas-chyly.com>
2022-03-04 19:35:42 +0100

    Fix MacOS clipping label

--- lib/ui/dataWidgets/ProjectDetailDataWidget.dart
+++ lib/ui/dataWidgets/ProjectDetailDataWidget.dart
@@ -238,6 +238,7 @@ class ProjectDetailDataWidgetState extends AbstractDataWidgetState<ProjectDetail
                                 ),
                               ),
                             ],
+                            clipBehavior: Clip.none,
                           ),
                         ),
                         CommonSpaceH(),
